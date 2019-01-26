33a25a898eb37b5fc7ebdc2ca498c7428c002182
livantorres <43475088+livantorres@users.noreply.github.com>
2019-01-25 19:18:13 -0500

    Add files via upload

---
+++ UAES/vista/contenido_vista.php
@@ -0,0 +1,35 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+
+
+
+<?php include 'plantillas/header.php'; ?>
+	<section class="main">
+		<div class="wrapp">
+			<?php include 'plantillas/nav.php'; ?>
+				<article>
+					<div class="mensaje">
+						<h2>Peluqueria Luisana | BarBerShop</h2>
+					</div>
+						<p><img src="img/spa.png">
+						</p><br/><br/>
+						<p>Bienvenidos a la aplicacion. Este software cubre todas las caracteristicas basicas para llevar un control de ventas, inventario y citas.</p>
+						<br/><br/>
+						<h3>Entre las caracteristicas de la plataforma tenemos: </h3><br/>
+						<p>	- Gestion de Citas <br/>
+							- Gestion de Clientestes <br/>
+							- Gestion de Usuarios con Acceso al Sistema <br/>
+							- Reporte e informes de citas medicas
+							- Reporte de Inventario
+						</p>
+				</article>
+	</section>
+	<?php include 'plantillas/footer.php'; ?>
+</body>
+</html>
---
+++ UAES/vista/login.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html >
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=decive-width, user-scalable=no, initial-scale=1.0, maximun-scale=1.0, minimun-scale=1.0">
+    <title>INICIAR SESION</title>
+    
+    <link rel="stylesheet" href="plantlogin/estilos.css">
+
+    <link rel="stylesheet" href="plantlogin/estilos1.css">
+
+    <link rel="stylesheet" href="plantlogin/bootstrap.css">
+    <link rel="icon" type="icon" href="../img/logo.png">
+
+   
+	
+</head>
+
+<body>
+
+
+
+
+
+      <?php include 'plantlogin/header.php'; ?>
+  <section class="main">
+    <div class="wrapp">
+      <?php include 'plantlogin/nav.php'; ?>
+        <article>
+          <div class="mensaje">
+            <h2>UAES | Unicor</h2>
+          </div>
+
+            
+            </p><br/><br/>
+            <p>Bienvenidos a la aplicacion. Este software cubre todas las caracteristicas basicas para llevar un control de citas.</p>
+            <br/><br/>
+            <h3>Entre las caracteristicas de la plataforma tenemos: </h3><br/>
+            <p> 
+              - Gestion de Citas <br/>
+              
+              - Acceso a la base de datos <br/>
+              - Reporte e informes de citas 
+              
+            </p>
+        </article>
+  </section>
+  <?php include 'plantillas/footer.php'; ?>
+
+	
+</body>
+
+</html>
